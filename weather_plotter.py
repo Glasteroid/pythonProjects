6fb3596896a6b722c337e14585ef8862a09c4efa
Glasteroid <kennethangeles880@yahoo.com>
2024-03-25 12:18:22 -0700

    Update weather_plotter.py

--- weather_plotter.py
+++ weather_plotter.py
@@ -3,13 +3,17 @@ import numpy as np
 from pylab import *
 
 print("Welcome to the weather plotter application!")
-user_input = input("Please choose a city you would like to obtain weather data from: ")
+user_city = input("Please choose a city you would like to obtain weather data from: ")
+user_date = input("Please choose a date in the city you chose you would like to obtain the hourly weather forecast from in the form (Year-Month-Day): ")
 
 params = {
-    'api_key': '799b1e124f6928e93849e430a7d2ea25',
-    'query': user_input
+    'access_key': '799b1e124f6928e93849e430a7d2ea25',
+    'query': user_city,
+    'historical_date': user_date
 }
 
-api_result = requests.get('https://api.weatherstack.com/current', params)
+api_result = requests.get('http://api.weatherstack.com/historical', params)
 
 api_response = api_result.json()
+
+print(api_response)
